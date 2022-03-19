25cfc309222979c5e6cb629f0d622c11e257ae2f
Antoine <lejugeti@gmail.com>
2022-03-19 21:24:47 +0100

    enable cors

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var videosRouter = require("./routes/videos.routes");
 
@@ -13,6 +14,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// CORS
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // SPECIFY ROUTES
 app.use("/videos", videosRouter);
