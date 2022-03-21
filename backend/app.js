30a5886a26ddfd0d7f362eec2c0780117cad91f6
Antoine <lejugeti@gmail.com>
2022-03-22 00:02:21 +0100

    log d'où vient la requête s'il y a une erreur

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,7 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
+  console.log(`Request from : ${req.origin}`);
   console.error(err);
 
   // set locals, only providing error in development
